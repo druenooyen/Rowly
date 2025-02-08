06c2d1dd6e08bf8dee2ab13a6ca17794d3bf7b19
druenooyen <75636415+druenooyen@users.noreply.github.com>
2025-02-07 17:28:28 -0800

    Added specification for RowLogbook class

---
+++ src/main/model/RowLogbook.java
@@ -0,0 +1,50 @@
+package model;
+
+// A class representing the logbook of rowing workout entries
+public class RowLogbook {
+
+    // EFFECTS: Creates logbook to store workout entries
+    public RowLogbook() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds workout entry to the row logbook
+    public void addRowEntry() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes workout entry from the row logbook
+    public void removeRowEntry() {
+        // stub
+    }
+
+    // EFFECTS: Returns total number of logbook entries
+    public int findTotalWorkouts() {
+        return 0; // stub
+    }
+
+    // EFFECTS: Returns total distance of all logbook entries
+    public int findTotalDistance() {
+        return 0; // stub
+    }
+
+    // EFFECTS: Returns total time of all logbook entries
+    public int findTotalTime() {
+        return 0; // stub
+    }
+
+    // REQUIRES: A 2km workout entry in the logbook
+    // EFFECTS: Returns best 2km time in workout logbook
+    public int find2kmPersonalBest() {
+        return 0; // stub
+    }
+
+    // REQUIRES: A 6km workout entry in the logbook
+    // EFFECTS: Returns best 6km time in workout logbook
+    public int find6kmPersonalBest() {
+        return 0; // stub
+    }
+
+}
