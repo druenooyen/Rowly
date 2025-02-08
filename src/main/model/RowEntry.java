d44ff6f4605f0ad6fcb2f22ab37d18448ca89cab
druenooyen <75636415+druenooyen@users.noreply.github.com>
2025-02-07 17:27:36 -0800

    Added specification for RowEntry class

---
+++ src/main/model/RowEntry.java
@@ -0,0 +1,61 @@
+package model;
+
+// A class representing a rowing workout entry having a date, distance (in meters), time, and flag status
+public class RowEntry {
+
+    // REQUIRES: date have the format yyyy-mm-dd
+    // distance > 0
+    // time have the format hh:mm:ss
+    // EFFECTS: constructs a workout entry for given date, distance, duration,
+    // and sets the flagged status to false
+    public RowEntry(String date, int distance, int duration) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets workout entry flag status to true
+    public void flagWorkout() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets workout entry flag status to false
+    public void unflagWorkout() {
+        // stub
+    }
+
+    // getters
+    public boolean getFlagStatus() {
+        return false; // stub
+    }
+
+    public String getDate() {
+        return null; // stub
+    }
+
+    public int getDuration() {
+        return 0; // stub
+    }
+
+    public int getDistance() {
+        return 0; // stub
+    }
+
+    // setters
+    public boolean setFlagStatus() {
+        return false; // stub
+    }
+
+    public String setDate() {
+        return null; // stub
+    }
+
+    public int setDuration() {
+        return 0; // stub
+    }
+
+    public int setDistance() {
+        return 0; // stub
+    }
+
+}
