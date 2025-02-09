d42f330db873d47c95f2f6a8527581787d327c02
druenooyen <75636415+druenooyen@users.noreply.github.com>
2025-02-09 14:09:40 -0800

    Added specification for RowTrackerApp

---
+++ src/main/ui/RowTrackerApp.java
@@ -0,0 +1,61 @@
+package ui;
+
+// Creates an instance of rowing workout tracker application
+public class RowTrackerApp {
+
+    // EFFECTS: runs the rowing workout tracker application
+    public RowTrackerApp() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: inititalizes the application with starting values
+    private void init() {
+        // stub
+    }
+
+    // EFFECTS: processes user input to the menu
+    private void processMenuCommands() {
+        // stub
+    }
+
+    // EFFECTS: displays menu options to user (Add entry, view all entries,
+    // view stats, view personal bests, quit)
+    private void displayMenuOptions() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Creates new RowEntry and adds to Row Logbook
+    private void addNewEntry() {
+        // stub
+    }
+
+    // EFFECT: prints all workout entries in log to the screen
+    private void printLogbook() {
+        // stub
+    }
+
+    // EFFECT: prints total workouts, total distance, and total time
+    //         of all entries in logbook
+    private void printLogbookTotals() {
+        // stub
+    }
+
+    // EFFECT: prints current 2k and 6k personal bests
+    private void printPersonalBests() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: prints closing message and sets program to not running
+    public void quitApp() {
+        // stub
+    }
+
+    // EFFECTS: prints a divider line of dashes
+    private void printDivider() {
+        // stub
+    }
+
+}
